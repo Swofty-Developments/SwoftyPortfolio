ff1f33234963e4134009fb04d989a5b2d31054f1
Swofty <admin@swofty.net>
2022-08-22 10:26:58 +1000

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,8 +59,8 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Ahmed.</h2>;
-  const three = <h3 className="big-heading">I'm a Bitch</h3>;
+  const two = <h2 className="big-heading">Swofty.</h2>;
+  const three = <h3 className="big-heading">I'm a Software Developer</h3>;
   const four = (
     <>
       <p>
